6a9f5a086091eaf6a1c970314211848acf0b9b6d
norlynj <nacastillo2@up.edu.ph>
2023-04-18 10:13:39 +0800

    add buttons on green box

--- src/com/view/InputPanel.java
+++ src/com/view/InputPanel.java
@@ -16,12 +16,16 @@ import java.awt.*;
 
 public class InputPanel extends Panel {
     private ImageButton musicOnButton, musicOffButton, homeButton;
+    private ImageButton pNPlus, pNMinus, aRNPlus, aRNMinus, runButton;
+    private JTextField processNumField, availableReourcesNumField;
+
 
 
     public InputPanel() {
 
         super("bg/input-panel.png");
 
+        // nav buttons
         musicOnButton = new ImageButton("buttons/volume-on.png");
         musicOffButton = new ImageButton("buttons/volume-off.png");
         homeButton = new ImageButton("buttons/home.png");
@@ -32,19 +36,61 @@ public class InputPanel extends Panel {
 
         musicOffButton.setVisible(false);
 
+        // green box area
+        processNumField = new JTextField("3", 2);
+        processNumField.setName("processNumField");
+        processNumField.setBorder(null);
+        processNumField.setHorizontalAlignment(SwingConstants.CENTER);
+        processNumField.setFont(new Font("Montserrat", Font.BOLD, 20));
 
+        pNPlus = new ImageButton("buttons/add.png");
+        pNMinus = new ImageButton("buttons/minus.png");
+        processNumField.setBounds(144, 217, 139, 42);
+        pNMinus.setBounds(122, 217, 44, 44);
+        pNPlus.setBounds(275, 217, 44, 44);
+
+
+        availableReourcesNumField = new JTextField("3", 2);
+        availableReourcesNumField.setName("processNumField");
+        availableReourcesNumField.setBorder(null);
+        availableReourcesNumField.setHorizontalAlignment(SwingConstants.CENTER);
+        availableReourcesNumField.setFont(new Font("Montserrat", Font.BOLD, 20));
+
+        aRNPlus = new ImageButton("buttons/add.png");
+        aRNMinus = new ImageButton("buttons/minus.png");
+        availableReourcesNumField.setBounds(468, 217, 192, 44);
+        aRNMinus.setBounds(446, 217, 44, 44);
+        aRNPlus.setBounds(599, 217, 139, 44);
+
+        runButton = new ImageButton("buttons/run.png");
+        runButton.setBounds(832, 218, 94, 42);
+
+
+        // Table
         setListeners();
 
+        this.add(pNMinus);
+        this.add(pNPlus);
+        this.add(aRNMinus);
+        this.add(aRNPlus);
+        this.add(processNumField);
+        this.add(availableReourcesNumField);
         this.add(musicOnButton);
         this.add(musicOffButton);
         this.add(homeButton);
+        this.add(runButton);
 
     }
 
     private void setListeners() {
+        pNMinus.hover("buttons/minus-hover.png", "buttons/minus.png");
+        pNPlus.hover("buttons/add-hover.png", "buttons/add.png");
+        aRNMinus.hover("buttons/minus-hover.png", "buttons/minus.png");
+        aRNPlus.hover("buttons/add-hover.png", "buttons/add.png");
         musicOnButton.hover("buttons/volume-off-hover.png", "buttons/volume-on.png");
         musicOffButton.hover("buttons/volume-on-hover.png", "buttons/volume-off.png");
         homeButton.hover("buttons/home-hover.png", "buttons/home.png");
+        runButton.hover("buttons/run-hover.png", "buttons/run.png");
     }
 
     public static void main(String[] args) {
