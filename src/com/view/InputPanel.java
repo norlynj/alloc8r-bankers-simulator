1f60e3108ec6e5400fee79849ff922cee1e764cc
norlynj <nacastillo2@up.edu.ph>
2023-04-19 23:06:13 +0800

    return BankersAlgorithm object from InputPanel class

--- src/com/view/InputPanel.java
+++ src/com/view/InputPanel.java
@@ -150,8 +150,6 @@ public class InputPanel extends Panel {
                 processNumField.setText(String.valueOf(Integer.parseInt(processNumField.getText()) - 1));
             }
         });
-        runButton.addActionListener( e -> run());
-        
         listenToUserInput();
     }
 
@@ -241,19 +239,13 @@ public class InputPanel extends Panel {
         return true;
     }
 
-    private BankersAlgorithm run() {
+    public BankersAlgorithm getBankers() {
         BankersAlgorithm banker = new SafetyAlgorithm();
         String processName;
 
         //get available and resource request
         int[] available = new int[Integer.parseInt(availableResourcesNumField.getText())];
         int[] request = new int[Integer.parseInt(availableResourcesNumField.getText())];
-        for (int i = 0; i < available.length; i++) {
-            available[i] = (int) availableTableModel.getValueAt(0, i);
-            request[i] = (int) requestResourceTableModel.getValueAt(0, i);
-        }
-        banker.setAvailableResources(available);
-        banker.setRequestResource(request);
 
         //get allocation and max
         for (int i = 0; i < allocationTableModel.getRowCount(); i++) {
@@ -261,15 +253,16 @@ public class InputPanel extends Panel {
             int[] max = new int[available.length];
             processName = (String) processTableModel.getValueAt(i, 0);
             for (int j = 0; j < allocation.length; j++) {
+                available[j] = (int) availableTableModel.getValueAt(0, j);
+                request[j] = (int) requestResourceTableModel.getValueAt(0, j);
                 allocation[j] = (int) allocationTableModel.getValueAt(i, j);
                 max[j] = (int) maxTableModel.getValueAt(i, j);
             }
             banker.setResourcesNumber(Integer.parseInt(processNumField.getText()));
             banker.add(new Process(processName, allocation, max));
         }
-        for (int i = 0; i < banker.getProcesses().size(); i++) {
-            System.out.println(banker.getProcesses().get(i).getProcessName());
-        }
+        banker.setAvailableResources(available);
+        banker.setRequestResource(request);
         banker.simulate();
         return banker;
     }
